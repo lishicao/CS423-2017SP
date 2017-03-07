63d7942d78d87679730f57bf7e2982895085496a
Lei <dlei5@sp17-cs423-10.cs.illinois.edu>
2017-03-06 21:21:56 -0600

    working ver

--- MP2/mp2.c
+++ MP2/mp2.c
@@ -54,6 +54,10 @@ struct mp_task_struct
 int flag = 1;
 #define DEBUG 1
 
+/*
+ * Find Mp Task Struct by PID
+ * this iterate through the list
+ */
 struct mp_task_struct *__get_task_by_pid(int pid)
 {
     struct mp_task_struct *tmp;
@@ -110,7 +114,7 @@ handle 3 cases based on the information passed in by userapp
 */
 static ssize_t mp_write (struct file *file, const char __user *buffer, size_t count, loff_t *data)
 {
-  printk("ENTERED WRITE\n");
+  printk("ENTERED WRITE FUNCTION\n");
   char* buf;
   buf = (char*) kmalloc(count+1, GFP_KERNEL);
   copy_from_user(buf, buffer, count);
@@ -166,18 +170,13 @@ void registration(char* buf) {
   new_task = (struct mp_task_struct*) kmem_cache_alloc(mp_task_struct_cache, GFP_KERNEL);
   INIT_LIST_HEAD(&new_task->task_node);
 
-printk(KERN_ALERT "created mp_task_struct address is %x\n", new_task);
+  printk(KERN_ALERT "created mp_task_struct address is %x\n", new_task);
   //init variables
   sscanf(buf+3, "%d, %d, %d\n", &new_task->pid, &new_task->period, &new_task->processing_time);
   new_task->task_state = SLEEPING;
   new_task->next_period = 0;
   new_task->task = find_task_by_pid(new_task->pid);
-
-//  printk("new_task->task: %x\n", new_task->task);
-
   setup_timer( &new_task->task_timer, _timer_callback, new_task->pid); 
-//  printk("mmp timer %x\n", &(new_task->task_timer));
-//  printk("registering %d: %d, %d\n", new_task->pid, new_task->period, new_task->processing_time);
 
   // admission control
   if (!admission_control(new_task)) return;
@@ -187,7 +186,7 @@ printk(KERN_ALERT "created mp_task_struct address is %x\n", new_task);
   list_add(&new_task->task_node, &head);
   spin_unlock(&mylock);
   // critical section end
-printk(KERN_ALERT "registratin succesfully finished\n");
+  printk(KERN_ALERT "registratin succesfully finished\n");
 }
 
 
@@ -206,15 +205,8 @@ void de_registration(char* buf)
   spin_lock(&mylock);
   struct mp_task_struct *task_to_remove = __get_task_by_pid(pid);
   task_to_remove->task_state = SLEEPING;
-  //printk("DELETE NODE: %x\n", task_to_remove->task_node); 
   del_timer( &task_to_remove->task_timer );
   list_del(&(task_to_remove->task_node));
-/*
-struct mp_task_struct* entry;
-  list_for_each_entry(entry, &head, task_node) {
-printk("%d[%d]: %d ms, %d ms\n", entry->pid, entry->task_state, entry->period, entry->processing_time);
-  }
-*/
 
   if(running_mptask == task_to_remove)
   {
@@ -223,7 +215,6 @@ printk("%d[%d]: %d ms, %d ms\n", entry->pid, entry->task_state, entry->period, e
   }
   //clean up
   kmem_cache_free(mp_task_struct_cache, task_to_remove);
-printk("de-reg unlocked\n");
   spin_unlock(&mylock);
   // critical section end
 }
@@ -235,16 +226,13 @@ wake up the dispatching thread
 void _timer_callback( int pid )
 {
   printk("entered timer callback, pid is %d\n", pid);
-  //return;
   unsigned long flags;
   spin_lock_irqsave(&mylock, flags);
   struct mp_task_struct *task_to_wake = __get_task_by_pid(pid);
-  printk("Timer Callback, %x\n", task_to_wake);
   task_to_wake->task_state = READY;
   if(task_to_wake!=NULL)
   {
     printk("TASK is NOT NULL\n");
-    //mod_timer( &task_to_wake->task_timer, jiffies + msecs_to_jiffies(task_to_wake->period) ); 
   }
   else
   {
@@ -252,70 +240,50 @@ void _timer_callback( int pid )
   }
   spin_unlock_irqrestore(&mylock, flags);
   wake_up_process(dispatcher);
-  // set_task_state(task_to_wake->task, TASK_RUNNING);
-
-  //set_current_state();
-  //schedule();
 }
 
+/*
+ * set the task priority helper
+ *
+ */
 void __set_priority(struct mp_task_struct *mytask, int policy, int priority)
 {
   struct sched_param sparam;
   sparam.sched_priority = priority;
-  printk("%x LINE %d\n", mytask->task, __LINE__);
   sched_setscheduler(mytask->task, policy, &sparam);
 }
+
 /**
 dispatching thread
 wake up in 2 cases: userapp signals YIELD; or timer interrupt
 perform the preemption, select and arrange the next thread to run
 */
 int thread_fn() {
-
-
-  printk(KERN_INFO "I'm at dispatcher function line number %d\n",__LINE__);
   struct mp_task_struct *task_to_run;
 
   // the ktread_should_stop will be changed flag at the module exit, where the kthread_stop() is called. 
   while(1){
-  
     set_current_state(TASK_INTERRUPTIBLE);
     schedule();
-    printk(KERN_INFO "I'm at dispatcher function line number %d\n",__LINE__);
     if(kthread_should_stop()) return 0;
     spin_lock(&mylock);
 
-printk("thread_fn get the lock\n");
     // loop over the task entries and find the ready task with smallest period
     struct mp_task_struct *entry;
     task_to_run = NULL;
     int min_period=INT_MAX;
-printk("min_period: %d\n", min_period);
-    printk(KERN_INFO "I'm at dispatcher function line number %d\n",__LINE__);
-
 
     list_for_each_entry(entry, &head, task_node)
     {
-
-//printk("entry addr: \n");
       if(entry->task_state == READY && entry->period < min_period ){
           task_to_run = entry;
           min_period = entry->period;
       }
-
-/*
-      if (entry->task_state != READY) continue;
-      if (task_to_run == NULL) task_to_run = entry;
-      else if (task_to_run->period > entry->period) task_to_run = entry;
-*/
     }
-
-    printk(KERN_INFO "I'm at dispatcher function line number %d\n",__LINE__);
 
     if( task_to_run == NULL  ) 
     {
       //no task is ready
-      printk(KERN_INFO "task_to_run is NULL\n");
       if(running_mptask != NULL)
       {
         printk("no task is ready\n");
@@ -324,75 +292,18 @@ printk("min_period: %d\n", min_period);
     }
     else 
     {
-printk("task_to_run addr: %x\n", task_to_run);
-printk("task_to_run->task addr: %x, equals null: %d\n", task_to_run->task, task_to_run->task==NULL);
-    printk("TASK TO RUN %d\n", task_to_run->task->pid);
       if(running_mptask != NULL && task_to_run->period < running_mptask->period)
       {
         running_mptask->task_state = READY;
         __set_priority(running_mptask, SCHED_NORMAL, 0);
       }
-    printk(KERN_INFO "I'm at dispatcher function line number %d\n",__LINE__);
-      
 
       task_to_run->task_state = RUNNING;
-      printk(KERN_INFO "I'm at dispatcher function line number %d\n",__LINE__);
       wake_up_process(task_to_run->task);
-      printk(KERN_INFO "I'm at dispatcher function line number %d\n",__LINE__);
       __set_priority(task_to_run, SCHED_FIFO, 99);
-      printk(KERN_INFO "I'm at dispatcher function line number %d\n",__LINE__);
       running_mptask = task_to_run;
-printk(KERN_INFO "I'm at dispatcher function line number %d\n",__LINE__);
     }
     spin_unlock(&mylock);
-  
-    printk(KERN_INFO "I'm at dispatcher function line number %d\n",__LINE__);
-      
-
-/*
-      spin_lock(&mylock);
-  printk(KERN_INFO "I'm at dispatcher function line number %d\n",__LINE__);
-      // set the previous running task state to ready and put to normal schedule
-      struct sched_param sparam;
-      if (running_mptask !=NULL){
-        sparam.sched_priority=0;
-        running_mptask->task_state = READY;
-        sched_setscheduler(running_mptask->task, SCHED_NORMAL, &sparam);
-        running_mptask = NULL;
-  printk(KERN_INFO "I'm at dispatcher function line number %d\n",__LINE__);
-      } 
-      // loop over the task entries and find the ready task with smallest period
-      struct mp_task_struct *entry;
-      struct mp_task_struct * return_task=NULL;
-  printk(KERN_INFO "I'm at dispatcher function line number %d\n",__LINE__);
-      long minp=LONG_MAX;
-  printk(KERN_INFO "I'm at dispatcher function line number %d\n",__LINE__);
-      list_for_each_entry(entry, &head, task_node)
-      { 
-        if(entry->task_state == READY && entry->period<minp ){
-            return_task = entry->task;
-            minp=entry->period;
-        }
-      }
-      // if there is a ready task, set it to running, install the timer
-      if (return_task!=NULL){
-      	return_task->task_state = RUNNING;
-      	running_mptask = return_task;
-      	setup_timer( &return_task->task_timer, _timer_callback, return_task->pid); 
-      }     
-      spin_unlock(&mylock);
-     
-      // if no ready task, then goto next loop, which sleeps at the beginning 
-      if (return_task ==NULL){
-        printk(KERN_INFO "no task in ready state\n");
-        continue;
-      }
-      
-      // wake up the returned task, set the priority and put it to real time scheduling
-      wake_up_process(return_task);
-      sparam.sched_priority=99;
-      sched_setscheduler(return_task, SCHED_FIFO, &sparam);
-*/
   }
 
   return 0;
@@ -410,10 +321,6 @@ void yield_handle(char* buf)
   sscanf(buf+3, "%d\n", &pid);
 
   struct mp_task_struct *task_to_yield = __get_task_by_pid(pid);
-  printk(KERN_ALERT "FOUND TASK TO YIELD  %d pid is %d\n", __LINE__, (task_to_yield->task)->pid);
-  
-
-  //if(task_to_yield->next_period==0) should_skip=0;
 
   task_to_yield->next_period += task_to_yield->next_period==0 ? 
     jiffies + msecs_to_jiffies(task_to_yield->period) :
@@ -427,29 +334,16 @@ void yield_handle(char* buf)
 
   //setup timer
   mod_timer(&(task_to_yield->task_timer), task_to_yield->next_period);
-//printk("AFTER MOD mmp timer %lu\n", task_to_yield->task_timer.expires);
   task_to_yield->task_state = SLEEPING;
 
   //set current running to null
   running_mptask = NULL;
-  //printk("Yield - My PID is %d", (int)getpid());
-  printk(KERN_ALERT "about to wake up dispacher  %d pid is %d\n", __LINE__, (task_to_yield->task)->pid);
 
   //wakeup dispatcher and schedule
   wake_up_process(dispatcher);
   set_task_state(task_to_yield->task, TASK_INTERRUPTIBLE);
 
-printk("Yielding----%d[%d]: %d ms, %d ms\n", task_to_yield->pid, task_to_yield->task_state, task_to_yield->period, task_to_yield->processing_time);
-
-//printk("----task_to_yield->task: %x\n", task_to_yield->task);
-
-printk(KERN_ALERT "I'm at yield_handle line  %d\n", __LINE__);
   schedule();
-printk(KERN_ALERT "I'm at yield_handle line  %d\n", __LINE__);
-  //wake_up_process(dispatcher);
-printk(KERN_ALERT "I'm at yield_handle line  %d\n", __LINE__);
-
-  //return 0;
 }
 
 static const struct file_operations mp_file = {
@@ -472,18 +366,12 @@ int __init mp_init(void)
   spin_lock_init(&mylock);
 
   // intialize slab allocator
-  printk("START ALLOCATE CACHE FOR SLAB\n");
   mp_task_struct_cache = KMEM_CACHE(mp_task_struct, SLAB_PANIC);
 
   // create dispatcher kernel thread
-  printk(KERN_INFO "dispatching thread init\n");
-  //dispatcher = kthread_create(thread_fn,NULL,"dispatcher");
   dispatcher = kthread_run(thread_fn,NULL,"dispatcher");
-
-printk("head addr: %x\n", head);
   
   printk(KERN_ALERT "MP MODULE LOADED\n");
-
   return 0;   
 }
 
@@ -497,7 +385,6 @@ void __exit mp_exit(void)
   #ifdef DEBUG
   printk(KERN_ALERT "MP1 MODULE UNLOADING\n");
   #endif
-  //struct list_head* n = head.next; 
 
 	struct mp_task_struct *entry;
 	struct mp_task_struct *temp_entry;
